1a0471515cddf115d27e6e04d388bf85a16c974a
dhruv-arora2019 <66963227+dhruv-arora2019@users.noreply.github.com>
2021-05-02 18:02:36 +0530

    Delete reminder.py

--- reminder.py
+++
@@ -1,9 +0,0 @@
-#from virtual import speak
-import mysql.connector
-import os
-
-def reminder ():
-    mydb=mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="1234")
